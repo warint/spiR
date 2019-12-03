70094b3ec7c1af7fa4b854d8c68f0d00e8e04900
Thierry Warin <thierry@nuance-r.com>
2019-12-03 13:45:41 +0000

    u

---
+++ R/spiR.R
@@ -0,0 +1,57 @@
+# Function 1: Data collection
+
+spi_data <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1_nQ9mQU_4J0KDRc4_TMzTsJHMYBqLwwnPaMC5BVhkGc/edit#gid=0")
+
+data_long <- reshape2::melt(SPI_data,
+  # ID variables - all the variables to keep but not split apart on
+  id.vars = c("countryName", "code", "year"),
+  # The source columns
+  measure.vars = colnames(SPI_data)[6:ncol(SPI_data)],
+  # Name of the destination column that will identify the original
+  # column that the measurement came from
+  variable.name = "indicator",
+  value.name = "value"
+)
+
+
+# Creating the default values for the function query
+# IF an entry is missing, all the observations of this variable will be displayed
+
+data_long_country <- base::unique(data_long[, 2])
+data_long_year <- utils::unique(data_long[, 3])
+data_long_indicator <- utils::unique(data_long[, 4])
+
+sqs_spi_data <- function(country = data_long_country,
+                         years = data_long_year,
+                         indicators = data_long_indicator) {
+  out <- dplyr::filter(data_long,
+                       code %in% country,
+                       year %in% years,
+                       indicator %in% indicators)
+  return(out)
+}
+
+# Function 2: Indicators' symbols query
+# If the user does not know the code of an indicator, s.he has access to the answer in natural language through this query
+
+sqs_spi_indicator <- function(indicators) {
+  spi_indicators_natural_language <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1_nQ9mQU_4J0KDRc4_TMzTsJHMYBqLwwnPaMC5BVhkGc/edit#gid=400714513")
+  if (missing(indicators)) {
+    spi_indicators_natural_language
+  } else {
+    spi_indicators_natural_language[grep(indicators, spi_indicators_natural_language$indicator_name), ]
+  }
+}
+
+# Function 3: Countries' code reconciliation
+# If the user does not know the ISO code of a country, s.he has access to the answer in natural language through this query
+
+sqs_spi_country <- function(country) {
+  spi_data <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1_nQ9mQU_4J0KDRc4_TMzTsJHMYBqLwwnPaMC5BVhkGc/edit#gid=0")
+  spi_countries_natural_language <- unique(spi_data[, 1:2])
+  if (missing(country)) {
+    spi_countries_natural_language
+  } else {
+    spi_countries_natural_language[grep(country, spi_countries_natural_language$countryName), ]
+  }
+}
