f54d0098656e6fbd31bb5c71ce888ecf7d841569
Thierry Warin <thierry@nuance-r.com>
2020-04-29 15:15:37 +0000

    u

--- R/spiR.R
+++ R/spiR.R
@@ -1,5 +1,5 @@
 
-#' sqs_spi_data
+#' spi_data
 #'
 #' @description This function allows you to find and display the Social Progress Index data according to the selected parameters.
 #' If no arguments are filled, all data will be displayed.
@@ -15,24 +15,24 @@
 #' @return Data for the country, indicator and year requested
 #' @export
 #'
-#' @seealso \code{\link{sqs_spi_indicator}} for the SPI's indicator symbol and \code{\link{sqs_spi_country}} for the country's ISO code.
+#' @seealso \code{\link{spi_indicator}} for the SPI's indicator symbol and \code{\link{spi_country}} for the country's ISO code.
 #'
 #' @examples
-#' myData<- sqs_spi_data(country = c("USA", "FRA"), years = "2018", )
-#' myData<- sqs_spi_data(country = c("USA", "FRA"), year = c("2018", "2019"), indicators = "SPI")
-#' myData<- sqs_spi_data("USA", "2019", c("SPI", "FOW"))
-#' myData<- sqs_spi_data(, "2018", )
-#' myData<- sqs_spi_data("USA", "2017", )
-#' myData<- sqs_spi_data("USA", , )
-#' myData<- sqs_spi_data(, , )
-#' myData<- sqs_spi_data()
+#' myData<- spi_data(country = c("USA", "FRA"), years = "2018", )
+#' myData<- spi_data(country = c("USA", "FRA"), year = c("2018", "2019"), indicators = "SPI")
+#' myData<- spi_data("USA", "2019", c("SPI", "FOW"))
+#' myData<- spi_data(, "2018", )
+#' myData<- spi_data("USA", "2017", )
+#' myData<- spi_data("USA", , )
+#' myData<- spi_data(, , )
+#' myData<- spi_data()
 #'
 #'
 #
 
 # Function 1: Data collection
 
-sqs_spi_data <- function(country = data_long_country,
+spi_data <- function(country = data_long_country,
                          years = data_long_year,
                          indicators = data_long_indicator) {
   var_code <- var_year <- var_indicator <- NULL
@@ -43,13 +43,13 @@ sqs_spi_data <- function(country = data_long_country,
   return(out)
 }
 
-spi_data <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1_nQ9mQU_4J0KDRc4_TMzTsJHMYBqLwwnPaMC5BVhkGc/edit#gid=0")
+spi_dataset <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1_nQ9mQU_4J0KDRc4_TMzTsJHMYBqLwwnPaMC5BVhkGc/edit#gid=0")
 
-data_long <- reshape2::melt(spi_data,
+data_long <- reshape2::melt(spi_dataset,
                             # ID variables - all the variables to keep but not split apart on
                             id.vars = c("countryName", "code", "year"),
                             # The source columns
-                            measure.vars = colnames(spi_data)[6:ncol(spi_data)],
+                            measure.vars = colnames(spi_dataset)[6:ncol(spi_dataset)],
                             # Name of the destination column that will identify the original
                             # column that the measurement came from
                             variable.name = "var_indicator",
@@ -71,7 +71,7 @@ data_long_indicator <- base::unique(data_long[, 4])
 # Function 2: Indicators' symbols query
 # If the user does not know the code of an indicator, s.he has access to the answer in natural language through this query
 
-#' sqs_spi_indicator
+#' spi_indicator
 #'
 #' @description This function allows you to find and search the right indicator code from the Social Progress Index you want to use.
 #' If no argument is filed, all indicators will be displayed.
@@ -79,16 +79,16 @@ data_long_indicator <- base::unique(data_long[, 4])
 #'
 #' @return Indicator code from the Social Progress Index.
 #' @export
-#' @seealso \code{\link{sqs_spi_country}} for the SPI's country code and \code{\link{sqs_spi_data}} to collect data when you have both indicator and country code.
+#' @seealso \code{\link{spi_country}} for the SPI's country code and \code{\link{spi_data}} to collect data when you have both indicator and country code.
 #'
 #' @examples
-#'myIndicator <- sqs_spi_indicator()
-#'myIndicator <- sqs_spi_indicator(indicators = "mortality")
-#'myIndicator <- sqs_spi_indicator("mortality")
+#'myIndicator <- spi_indicator()
+#'myIndicator <- spi_indicator(indicators = "mortality")
+#'myIndicator <- spi_indicator("mortality")
 #'
 
 
-sqs_spi_indicator <- function(indicators) {
+spi_indicator <- function(indicators) {
   spi_indicators_natural_language <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1_nQ9mQU_4J0KDRc4_TMzTsJHMYBqLwwnPaMC5BVhkGc/edit#gid=400714513")
   if (missing(indicators)) {
     spi_indicators_natural_language
@@ -108,16 +108,16 @@ sqs_spi_indicator <- function(indicators) {
 #'
 #' @return Country's ISO code.
 #' @export
-#' @seealso \code{\link{sqs_spi_indicator}} for the SPI's indicators and \code{\link{sqs_spi_data}} to collect data when you have both indicator and country code.
+#' @seealso \code{\link{spi_indicator}} for the SPI's indicators and \code{\link{spi_data}} to collect data when you have both indicator and country code.
 #' @examples
-#'mycountry <- sqs_spi_country()
-#'mycountry <- sqs_spi_country(country = "Canada")
-#'mycountry <- sqs_spi_country("Canada")
+#'mycountry <- spi_country()
+#'mycountry <- spi_country(country = "Canada")
+#'mycountry <- spi_country("Canada")
 #'
 
 
-sqs_spi_country <- function(country) {
-  spi_countries_natural_language <- unique(spi_data[, 1:2])
+spi_country <- function(country) {
+  spi_countries_natural_language <- unique(spi_dataset[, 1:2])
   if (missing(country)) {
     spi_countries_natural_language
   } else {
